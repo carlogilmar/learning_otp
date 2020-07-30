6011afe97188253aa27b990eb76a0278eaf89f57
Carlo Gilmar <carlogilmar12@gmail.com>
2020-07-30 11:08:17 -0500

    Adding metrics schema

---
+++ lib/learning_otp/accounts/metric.ex
@@ -0,0 +1,12 @@
+defmodule LearningOtp.Accounts.Metric do
+
+  use Ecto.Schema
+
+  @primary_key false
+  schema "metrics" do
+    field :date, :date, primary_key: true
+    field :path, :string, primary_key: true
+    field :counter, :integer, default: 0
+  end
+
+end
